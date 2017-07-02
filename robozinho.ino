c226ed6ba485326d82efafed3c10ad80dedb5602
lucassid <lucassidpacheco@gmail.com>
2017-07-02 18:58:35 -0300

    sketch das funcoes

--- robozinho.ino
+++ robozinho.ino
@@ -1,38 +1,92 @@
 /* RELÂMPAGO MARQUINHOS VERSÃO 0.0.1
- * AUTOR: LUCAS DE SOUSA PACHECO
- * MODIFICADO EM: 02/07/2017
- */
+   AUTOR: LUCAS DE SOUSA PACHECO
+   MODIFICADO EM: 02/07/2017
+*/
 
- #define slinha 3 // pino analógico
- #define hcTrigg 7 // trigger do sensor ultrassônico
- #define hcEcho 8 // echo do sensor ultrassônico
+#define slinha 3 // pino analógico
+#define hcTrigg 7 // trigger do sensor ultrassônico
+#define hcEcho 8 // echo do sensor ultrassônico
 
- /*-- PINOS PWM PARA CONTROLE DO MOTORES --*/
- #define m1Pin1 5 // In 1 do primeiro motor
- #define m1Pin2 6 // In 2 do primeiro motor
- #define m2Pin1 9 //Ind 1 segundo motor
- #define m2Pin2 10 //Ind 2 segundo motor
+/*-- PINOS PWM PARA CONTROLE DO MOTORES --*/
+#define m1Pin1 5 // In 1 do primeiro motor
+#define m1Pin2 6 // In 2 do primeiro motor
+#define m2Pin1 9 //Ind 1 segundo motor
+#define m2Pin2 10 //Ind 2 segundo motor
 
- /*-- INTERVALO DE CHAMADA DAS FUNÇÕES --*/
- int slTime = 50; //milissegundos
- int hcTime = 100; //milissegundos
+/*-- INTERVALO DE CHAMADA DAS FUNÇÕES --*/
+int slTime = 50; //milissegundos
+int hcTime = 100; //milissegundos
 
- /*-- VARIÁVEIS DE CONTROLE --*/
- bool inimigo = 0;
- bool linha = 0;
- // CONTROLE DO TEMPO
- unsigned long lastSLinha = 0;
- unsigned long lastHC = 0;
+/*-- VARIÁVEIS DE CONTROLE --*/
+bool inimigo = 0;
+bool linha = 0;
+int linhaCalibre = 0;
+int val = 0;
+// CONTROLE DO TEMPO
+unsigned long lastSLinha = 0;
+unsigned long lastHC = 0;
+long duration, distance;
 
- void procuraInimigos (){
-  
- }
+/*-- FUNÇÕES DE STATUS --*/
+void procuraInimigos () {
+  //emite um pulso e calcula a distância em cm de objetos pŕoximos
+  digitalWrite(hcTrigg, LOW);
+  delayMicroseconds(5);
+  digitalWrite(hcTrigg, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(hcTrigg, LOW);
+  duration = pulseIn(hcEcho, HIGH);
 
- void procuraLinha (){
-  
- }
+  //converte a distância para cm
+  distance = (duration / 2) / 29.1;
 
- void setup (){
+  if (distance < 30)
+    inimigo = true;
+  delay (50);
+}
+
+void procuraLinha () {
+  val = analogRead (slinha);
+
+  if (val - linhaCalibre > 10) { //determinhar treshold ainda
+    linha = 1;
+    return;
+  }
+  linha = 0;
+}
+
+/*--- FUNÇÕES DE MOVIMENTO ---*/
+void andaFrente (int pwd) {
+  analogWrite (m1Pin1, pwd);
+  digitalWrite(m1Pin2, LOW);
+  analogWrite (m2Pin1, pwd);
+  digitalWrite (m2Pin2, LOW);
+}
+
+void andaTras (int pwd) {
+  analogWrite (m1Pin2, pwd);
+  digitalWrite(m1Pin1, LOW);
+  analogWrite (m2Pin2, pwd);
+  digitalWrite (m2Pin1, LOW);
+}
+
+void giraHorario (bool sentido) {
+  //0 - horário
+  //1 - anti-horário
+  if (sentido) {
+    analogWrite (m1Pin2, 100);//valor a ajustar
+    digitalWrite(m1Pin1, LOW);
+    analogWrite (m2Pin1, 100);//valor a ajustar
+    digitalWrite (m2Pin2, LOW);
+    return;
+  }
+  analogWrite (m1Pin1, 100);//valor a ajustar
+  digitalWrite(m1Pin2, LOW);
+  analogWrite (m2Pin2, 100);//valor a ajustar
+  digitalWrite (m2Pin1, LOW);
+}
+
+void setup () {
   //inicialização dos pinos
   pinMode (hcTrigg, OUTPUT);
   pinMode (hcEcho, INPUT);
@@ -41,15 +95,16 @@
   pinMode (m2Pin1, OUTPUT);
   pinMode (m2Pin2, OUTPUT);
 
+  linhaCalibre = analogRead (slinha);
   //espera inicial
   delay (5000);
- }
+}
 
- void loop (){
+void loop () {
   if (millis() - lastSLinha >= slTime)
-    procuraLinha ();  
+    procuraLinha ();
 
   if (millis() - lastHC >= hcTime)
-    procuraInimigos ();  
- }
+    procuraInimigos ();
+}
 
